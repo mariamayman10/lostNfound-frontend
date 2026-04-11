2fbb9418045abb8ed3021e6c4b9acd5fbf1cc1b3
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-04-12 00:21:07 +0200

    update feedback card styling

--- src/components/feedbackList.jsx
+++ src/components/feedbackList.jsx
@@ -17,36 +17,38 @@ function FeedbackList() {
   }, []);
   if (isLoading)
     return (
-      <p className="text-center font-medium text-gray-400 py-5">Loading Feedbacks</p>
+      <p className="text-center font-medium text-gray-400 py-5">
+        Loading Feedbacks
+      </p>
     );
   return (
     <div className="bg-(--primary) text-(--text-color) py-12 px-4">
-      <div className="max-w-6xl mx-auto">
+      <div className="max-w-6xl m-auto">
         <h2 className="text-2xl font-semibold text-center mb-8">
           What People Say
         </h2>
 
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="flex justify-center gap-4 flex-wrap">
           {feedbacks.map((fb) => (
             <div
               key={fb.id}
-              className="bg-white text-(--primary) rounded-xl p-5 shadow-md hover:-translate-y-1 transition-transform"
+              className="bg-white text-(--primary) rounded-xl p-5 shadow-md hover:-translate-y-1 transition-transform w-68"
             >
-              <div className="flex items-center gap-3 mb-3">
+              <div className="flex flex-col items-center ">
                 <img
                   src={`${baseUrl}/users/profile/${fb.user.photoUrl.split("/")[3]}`}
                   alt={fb.user.name}
-                  className="w-11 h-11 rounded-full object-cover"
+                  className="w-20 h-20 rounded-full object-cover mb-4"
                 />
 
                 <div>
-                  <h4 className="font-semibold text-sm">{fb.user.name}</h4>
+                  <h4 className="font-semibold text-lg">{fb.user.name}</h4>
 
-                  <div className="flex gap-1 mt-1">
+                  <div className="flex gap-1 mt-1 justify-center">
                     {[1, 2, 3, 4, 5].map((star) => (
                       <FaStar
                         key={star}
-                        size={12}
+                        size={14}
                         className={
                           star <= fb.rating
                             ? "text-(--secondary)"
@@ -57,8 +59,8 @@ function FeedbackList() {
                   </div>
                 </div>
               </div>
-              <p className="text-sm leading-relaxed text-gray-700">
-                "{fb.message}"
+              <p className="text-md mt-8 leading-relaxed text-center text-gray-700">
+                {fb.message}
               </p>
             </div>
           ))}
